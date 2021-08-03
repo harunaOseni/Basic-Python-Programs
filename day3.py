35611b5a094c703c42e4801c61816e1b1b55509a
Haruna Oseni <harunaoseni23@gmail.com>
2021-08-03 09:40:09 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -341,3 +341,37 @@
 #         print("You win.")
 #     print(stages[lives])
 
+#ceaser cipher program 
+# from art import logo
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+#             'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# ended = False
+
+
+# def caesar(start_text, shift_amount, cipher_direction):
+#     end_text = ""
+#     if cipher_direction == "decode":
+#         shift_amount *= -1
+#     for char in start_text:
+#         if char not in alphabet:
+#             end_text += char 
+#         else: 
+#             position = alphabet.index(char) 
+#             new_position = position + shift_amount 
+#             end_text += alphabet[new_position] 
+#     print(f"Here's the {cipher_direction}d result: {end_text}")
+
+# print(logo)
+# ended = False
+
+# while not ended:
+#     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+#     text = input("Type your message:\n").lower()
+#     shift = int(input("Type the shift number:\n"))
+#     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+#     want_to_go_again = input("Do you want to go again? Type 'yes' or 'no':\n")
+#     if want_to_go_again == "no":
+#         ended = True
+
+# if shift > 26:
+#     shift = shift % 26
